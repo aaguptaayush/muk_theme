6dc181e1e286ac2e445692a9ca97a5f33ec2ab33
Ayush Gupta <ayush@iqminds.org>
2022-01-31 21:53:47 +0530

    add

--- hrms_dashboard/static/src/js/hrms_dashboard.js
+++ hrms_dashboard/static/src/js/hrms_dashboard.js
@@ -37,10 +37,10 @@ var HrDashboard = AbstractAction.extend({
 
     template: 'HrDashboardMain',
     cssLibs: [
-        '/hrms_dashboard/static/src/css/lib/nv.d3.css'
+        '/hrms_dashboard/static/src/css/nv.d3.css'
     ],
     jsLibs: [
-        '/hrms_dashboard/static/src/js/lib/d3.min.js'
+        '/hrms_dashboard/static/src/js/d3.min.js'
     ],
 
      events: {
